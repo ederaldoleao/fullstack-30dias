59523cb5a9582505c6e36b6605158d2276829062
Login <login@Logins-MacBook-Pro.local>
2026-02-10 22:16:22 +0000

    day 01: variables and arithmetic exercises completed

--- day-01.js
+++ day-01.js
@@ -1,3 +1,4 @@
+// Variáveis e tipos de dados
 let name = "Ederaldo";
 let age = 27;
 let studying = true;
@@ -6,6 +7,7 @@ console.log(name);
 console.log(age);
 console.log(studying);
 
+// Operações matemáticas
 let a = 10;
 let b = 5;
 
@@ -13,3 +15,51 @@ console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
+
+// Média de dois números
+let n1 = 7;
+let n2 = 9;
+let media = (n1 + n2) / 2;
+
+console.log("Média:", media);
+
+// 15% de 200
+let valor = 15;
+let total = 200;
+let resultado = (valor * total) / 100;
+
+console.log("15% de 200 é:", resultado);
+
+// Idade em dias
+let idade = 27;
+let dias = idade * 365;
+
+console.log("A idade em dias é:", dias);
+
+// Dobro e triplo
+let numero = 4;
+let obro = numero * 2;
+let triplo = numero * 3;
+
+console.log("Dobro:", dobro);
+console.log("Triplo:", triplo);
+
+// Salário com aumento de 10%
+let salario = 4000;
+let aumento = salario * 0.10;
+let novoSalario = salario + aumento;
+
+console.log("Novo salário com aumento de 10%:", novoSalario);
+
+// Km => m
+let km = 5;
+let m = km * 1000;
+
+console.log("Km em metros:", m);
+
+//Preço com desconto de 20%
+let preco = 150;
+let desconto = (preco * 20) / 100;
+let precoFinal = preco - desconto;
+
+console.log("Preço com desconto de 20%:", precoFinal);
