7e1d9e4e87a9b8a1f79c5e94a537c90b22110985
Login <login@Logins-MacBook-Pro.local>
2026-02-10 21:34:05 +0000

    day 01: variables and operators completed

---
+++ day-01.js
@@ -0,0 +1,15 @@
+let name = "Ederaldo";
+let age = 27;
+let studying = true;
+
+console.log(name);
+console.log(age);
+console.log(studying);
+
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
